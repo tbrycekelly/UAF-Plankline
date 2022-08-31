d6c7d326958ea372ad41fff93e969deac929a91f
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2022-08-31 11:19:42 -0800

    Working version

--- classification.py
+++ classification.py
@@ -51,9 +51,12 @@ import sys
 import shutil
 import argparse
 import glob
-
+import logging # TBK: logging module
+import configparser # TBK: To read config file
+import logging.config # TBK 
+import tqdm # TBK
 import subprocess
-from datetime import datetime
+import datetime
 from multiprocessing import Pool, Queue
 
 #----------------------------------------------------------------------------------------
@@ -76,65 +79,44 @@ def directory(arg):
 # Global Variables: scnn_directory, classification_dir, epoch, queue
 #
 def classify(tar_file):
-    sys.stdout.write('Starting classification...')
-    sys.stdout.flush()
-    print ('Start: classify')
-    stdout = sys.stdout
+    logger.info("Starting classify")
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-    today = datetime.now()
-    date = today.strftime("%Y-%m-%d")
-
-    image_dir = tar_file.replace(".tar", "") # remove extension
+    image_dir = tar_file.replace(".tar.gz", "") # remove extension
     tar_identifier = os.path.basename(image_dir)
-
-    log_file = "{classification_dir}/{tar_identifier}-{date}.log".format(classification_dir=classification_dir, tar_identifier=tar_identifier, date=date)
-
-    with open(log_file, 'w') as l:
-        sys.stdout = l 
-
-        gpu_id = queue.get()
-
-        print("Date:", date)
-        print("Starting on GPU {gpu_id}".format(gpu_id=gpu_id))
-
-        print ('image_dir: ', image_dir)
-        image_dir = tar_file.replace(".tar", "") # remove extension
-
-        print ('tar_identifier: ', tar_identifier)
-        tar_identifier = os.path.basename(image_dir)
-        
-        os.makedirs(image_dir, 0o777, exist_ok=True)
-        untar_cmd = 'tar --no-overwrite-dir -xf {tar_file} -C {image_dir} --strip-components=4 --wildcards "*.png"' # TBK change strip-components to what you need.
-
-        print ('untar_cmd: ', untar_cmd.format(tar_file=tar_file, image_dir=image_dir))
-        os.system(untar_cmd.format(tar_file=tar_file, image_dir=image_dir))
-
-        # Perform classification.
-        #
-        # untar_cmd = 'tar --no-overwrite-dir -xzf {tar_file} -C {image_dir} --strip-components=3 --wildcards "*.png"'
-        scnn_cmd  = "cd {scnn_dir}; ./isiis_scnn -start {epoch} -stop {epoch} -unl {image_dir} -cD {gpu_target} >> {log_file}"
-        
-        # print ('untar_cmd: ', untar_cmd)
-        print ('scnn_cmd: ', scnn_cmd.format(scnn_dir=scnn_directory, epoch=epoch, image_dir=image_dir, gpu_target=gpu_id, log_file=log_file))
-        print ('Start: SCNN')
-        os.system(scnn_cmd.format(scnn_dir=scnn_directory, epoch=epoch, image_dir=image_dir, gpu_target=gpu_id, log_file=log_file))
-        print ('End: SCNN')
-
-        # Move the csv file resulting from classification.
-        #
-        csv_path = glob.glob("{scnn_dir}/Data/plankton/*{tar_identifier}*".format(scnn_dir=scnn_directory, tar_identifier=tar_identifier))[0]
-        csv_file = "{classification_dir}/{tar_identifier}-{date}.csv".format(classification_dir=classification_dir, tar_identifier=tar_identifier, date=date)
-        shutil.move(csv_path, csv_file)
-
-        # Clean directories to make space.
-        #
-        shutil.rmtree(image_dir)
-
-        queue.put(gpu_id) # add the gpu id to the queue so that it can be allocated again
-
-    sys.stdout = stdout # switch back to standard stdout
+    os.makedirs(image_dir, permis, exist_ok = True)
     
-    print ('End: classify')
+    gpu_id = queue.get()
+
+    logger.info("Starting on GPU {gpu_id}".format(gpu_id=gpu_id))
+    logger.info(f'image_dir: {image_dir}')
+    logger.info(f'tar_identifier: {tar_identifier}')
+    
+    # Untar files
+    untar_cmd = 'tar --no-overwrite-dir -xzf {tar_file} -C {image_dir} --strip-components=4 --wildcards "*.png"' # TBK change strip-components to what you need.
+    logger.debug('Untarring files: ' + untar_cmd.format(tar_file = tar_file, image_dir = image_dir))
+    os.system(untar_cmd.format(tar_file = tar_file, image_dir = image_dir))
+
+    # Perform classification.
+    log_file = "{classification_dir}/{tar_identifier}-{date}.log".format(classification_dir = classification_dir, tar_identifier = tar_identifier, date = date)
+    scnn_cmd  = "cd {scnn_dir}; nohup ./isiis_scnn -start {epoch} -stop {epoch} -unl {image_dir} -cD {gpu_target} > {log_file} 2>&1"
+    
+    logger.debug('Running SCNN: ' + scnn_cmd.format(scnn_dir = scnn_directory, epoch = epoch, image_dir = image_dir, gpu_target = gpu_id, log_file = log_file))
+    logger.info('Start SCNN.')
+    
+    os.system(scnn_cmd.format(scnn_dir = scnn_directory, epoch = epoch, image_dir = image_dir, gpu_target = gpu_id, log_file = log_file))
+    logger.info('End SCNN.')
+
+    # Move the csv file resulting from classification.
+    csv_path = glob.glob("{scnn_dir}/Data/plankton/*{tar_identifier}*".format(scnn_dir=scnn_directory, tar_identifier=tar_identifier))[0]
+    csv_file = "{classification_dir}/{tar_identifier}-{date}.csv".format(classification_dir=classification_dir, tar_identifier=tar_identifier, date=date)
+    shutil.move(csv_path, csv_file)
+
+    # Clean directories to make space.
+    shutil.rmtree(image_dir)
+
+    queue.put(gpu_id) # add the gpu id to the queue so that it can be allocated again
+    logger.debug('End classify.')
 
 
 #----------------------------------------------------------------------------------------
@@ -143,35 +125,40 @@ def classify(tar_file):
 if __name__ == "__main__":
     # create a parser for command line arguments
     parser = argparse.ArgumentParser(description="Classification tool for managing the isiis_scnn processes")
-    parser.add_argument("-s", "--scratch_base", type=directory, required=True, help="This is the path for fast ssd device storage where the segmentation data is already")
-    parser.add_argument("-i", "--scnn_instances", type=int, default=2, required=True, help="Number of isiis_scnn instances to be run on a GPU")
-    parser.add_argument("-d", "--scnn_directory", type=directory, required=True, help="Directory containing the isiis_scnn executable and the epochs directory")
-    parser.add_argument("-e", "--epoch", type=int, required=True, help="Epoch weight file to be used for inference")
+    parser.add_argument("-c", "--config", required = True, help = "Configuration ini file.")
+    
 
     # read in the arguments
     args = parser.parse_args()
-    scratch_base = os.path.abspath(args.scratch_base)
-    scnn_instances = args.scnn_instances
-    scnn_directory = args.scnn_directory
-    epoch = args.epoch
-    chunksize = 1
+    
+    config = configparser.ConfigParser() # TBK
+    config.read(args.config) # TBK
+    
+    logging.config.fileConfig(config['logging']['config'], defaults={'date':datetime.datetime.now(),'path':config['general']['working_dir'],'name':'classification'}) # TBK
+    logger = logging.getLogger('sLogger') # TBK
+    
+    permis = int(config['general']['dir_permissions'])
+    scnn_instances = int(config['classification']['scnn_instances'])
+    scnn_directory = config['classification']['scnn_dir']
+    epoch = int(config['classification']['epoch'])
+    chunksize = int(config['classification']['chunksize'])
+    scratch_base = os.path.abspath(config['general']['working_dir'])
 
     # segmentation_dir is where the input data is taken from
     segmentation_dir = scratch_base + "/segmentation"  
     classification_dir = scratch_base + "/classification"
-    os.makedirs(classification_dir, 0o777,exist_ok=True)
+    os.makedirs(classification_dir, permis, exist_ok = True)
 
     # make sure this is a valid directory
     if not os.path.exists(segmentation_dir):
         sys.exit("No directory segmentation")
 
-    tars = [os.path.join(segmentation_dir, tar) for tar in os.listdir(segmentation_dir) if tar.endswith(".tar")]
+    tars = [os.path.join(segmentation_dir, tar) for tar in os.listdir(segmentation_dir) if tar.endswith(".tar.gz")]
     if len(tars) == 0:
         sys.exit("Error: No tars file in segmenation directory")
 
     # setup gpu queue
     num_gpus = str(subprocess.check_output(["nvidia-smi", "-L"])).count('UUID') # read number of gpus from nvidia-smi output
-    print("number of gpus", num_gpus)
 
     queue = Queue()
     for gpu_id in range(num_gpus):
@@ -179,9 +166,12 @@ if __name__ == "__main__":
             queue.put(gpu_id)
 
     num_processes = scnn_instances * num_gpus
-    print("scnn_instances", scnn_instances)
-    print("processes", num_processes)
-
+    tar_length = len(tars)
+    logger.debug(f"Number of GPUs: {num_gpus}")
+    logger.debug(f"SCNN Instances per GPU: {scnn_instances}")
+    logger.debug(f"Total Processes: {num_processes}")
+    logger.info(f"Identified {tar_length} tar.gz files")
+    
     # this is the parallel portion of the code
     p = Pool(num_processes)
 
@@ -190,9 +180,16 @@ if __name__ == "__main__":
     #
     # Start the Classification processes.
     #
-    p.imap_unordered(classify, tars, chunksize=chunksize)
-
+    #p.imap_unordered(classify, tars, chunksize = chunksize)
+    # TBK:
+    for _ in tqdm.tqdm(p.imap_unordered(classify, tars, chunksize=chunksize), total=len(tars)):
+        pass
+    #r = list(tqdm.tqdm(p.imap(classify, tars, chunksize = chunksize), total = len(tars)))
+    
     p.close()
     p.join() # blocks so that we can wait for the processes to finish
+    
+    logger.debug("Finished classification.")
+    print("Finished Classification.")
     
 
--- segmentation.py
+++ segmentation.py
@@ -55,18 +55,15 @@ import os
 import sys
 import shutil
 import argparse
+import logging # TBK: logging module
+import logging.config # TBK 
+import configparser # TBK: To read config file
+import tqdm # TBK
+
+from multiprocessing import Pool
 import datetime
 
-import subprocess
-from multiprocessing import Pool
-from measure_parallel import measure
 
-import time
-
-# directory
-#
-# custom arg type for argparse
-#
 def directory(arg):
     if os.path.isdir(arg):
         if arg.endswith("/"): 
@@ -75,66 +72,19 @@ def directory(arg):
     else:
         raise argparse.ArgumentTypeError("Not a valid directory path")
         
-#--------------------------------------------------------------------------
-# seg_ff
-#
-def seg_ff(avi, seg_output, SNR):
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': Start seg_ff')
-    print ('\tavi -->' + avi + '<--')
-    print ('\tseg_output -->' + seg_output + '<--')
-    print ('\tSNR -->' + str(SNR) + '<--')
+
+def seg_ff(avi, seg_output, SNR, segment_path):
+    snr = str(SNR)
+    epsilon = config['segmentation']['overlap']
+    segment_log = scratch_base + '/segmentation/segment_' + str(datetime.datetime.now()) + '.log'
+    full_output = config['segmentation']['full_output']
     
-    seg = 'segment -i {avi_file} -n 1 -o {out} -s {signal_noise_ratio} '
-    print ('\tseg.format -->' + seg.format(avi_file=avi, out=seg_output, signal_noise_ratio=str(SNR)) + '<--')
+    seg = f'nohup {segment_path} -i {avi} -n 1 -o {seg_output} -s {snr} -e {epsilon} {full_output} > "{segment_log}" 2>&1'
+    logger.info("Segmentation call: " + seg)
 
-    os.system (seg.format(avi_file=avi, out=seg_output, signal_noise_ratio=str(SNR)))
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': End seg_ff')
+    os.system(seg)
 
-#--------------------------------------------------------------------------
-# make_tifs
-#
-def make_tifs(avi, tif_structure):
-    print ('Start: make_tifs')
-    print ('avi -->' + avi + '<--')
-    print ('tif_structure -->' + tif_structure + '<--')
-    sys.stdout.flush()
 
-    # get video framerate
-    FFREPORT = "ffreport.log"
-    os.environ["FFREPORT"] = "file="+FFREPORT 
-    ffprobe = "ffprobe -select_streams v -show_streams {video} | grep r_frame_rate | sed -e 's/r_frame_rate=//'"
-
-    frame_rate = subprocess.check_output(ffprobe.format(video=avi), shell=True)
-    if (not frame_rate): # if no frame rate returned video is not verified
-        return False
-
-    numer, denom = frame_rate[:-1].decode("utf-8").split('/') # remove new line, decode bystring and split on forward slash
-    frame_rate = int(int(numer) / int(denom)) # change from fraction to int
-
-    print ('frame_rate -->' + str(frame_rate) + '<--', flush=True)
-    sys.stdout.flush()
-
-    ffmpeg = "ffmpeg -i {video} {output}" 
-    print (ffmpeg.format(video=avi, output=tif_structure))
-    os.system(ffmpeg.format(video=avi, output=tif_structure))
-
-    print ('End: make_tifs')
-    sys.stdout.flush()
-
-    return True
-
-#--------------------------------------------------------------------------
-# command_redo
-#
-def command_redo(cmd, err_message):
-    print ('Start: command_redo')
-    res = os.system(cmd)
-    for i in range(4): # retry transfer multiple times to make sure data is transfered
-        if(res == 0): # res == 0 means the last command was a success
-            break;
-        print(err_message)
-        res = os.system(cmd)
-    print ('End: command_repo')
 
 #--------------------------------------------------------------------------
 # local_main
@@ -143,14 +93,8 @@ def command_redo(cmd, err_message):
 # Necessary global variables: short_segment_scratch, SNR, fp_measure.
 #
 def local_main(avi):
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': Start local_main')
-      
-    # Global Data:
-    # short_segment_scratch: this is the place on the root of the machine where the seg_ff happens
-    # SNR: this is the Sound to Noise Ratio
-    # measurements to 
-    # remote_base
-
+    logging.info("Starting local_main.")
+    
     # setup all of the local paths for the avi
     avi_file = os.path.basename(avi) # get only the file part of the full path
     avi_date_code = os.path.splitext(avi_file)[0] # remove .avi
@@ -158,73 +102,53 @@ def local_main(avi):
     seg_output = avi_segment_scratch + "_s"
     tif_structure = avi_segment_scratch + "/" + avi_date_code + "_%04d.tif"
 
-    print ('avi_file:', avi_file)
-    print ('avi_date_code:', avi_date_code)
-    print ('avi_segment_scratch:', avi_segment_scratch)
-    print ('seg_output:', seg_output)
-    print ('tif_structure:', tif_structure)
-    print ('short_segment_scratch:', short_segment_scratch)
-    print ('scratch_base: ', scratch_base)
-
-    # Deals with the segmentation fault that results from having a output dir that is too long.
-    #max_segment_output = 37
-    #if (len(short_segment_scratch) > max_segment_output):
-    #    print("Exiting: Program will segmentation fault if the scratch path is too long.")  
-    #    exit()
-
-    # NOTE: since the raw avi files are so large we have opted to transfer them as they are needed
-    if (lazy_transfer):
-        if (remote_data):  # transfer the avi from cgrb
-            transfer = "rsync -azv -e 'ssh -p {port}' {remote_login}:{avi} {scratch}/".format(port=port, remote_login=remote_login, avi=avi,scratch=raw_scratch)
-            command_redo(transfer, "Trying to copy image over again")
-        else:
-            transfer = "rsync -azv {avi} {scratch}/".format(avi=avi, scratch=raw_scratch)
-            command_redo(transfer, "Trying to copy image over again")
-
-        avi = raw_scratch + "/" + avi_file # assign to original variable so the script can function properly
+    logging.info(f'avi_file: {avi_file}')
+    logging.info(f'avi_date_code: {avi_date_code}')
+    logging.info(f'avi_segment_scratch: {avi_segment_scratch}')
+    logging.info(f'seg_output: {seg_output}')
+    logging.info(f'tif_structure: {tif_structure}')
+    logging.info(f'short_segment_scratch: {short_segment_scratch}')
+    logging.info(f'scratch_base: {scratch_base}')
 
     # Printout to show progress.
     #
-    sys.stdout.write(avi_file + "\n")
-    sys.stdout.flush()
+    logging.debug(f'Starting AVI file: {avi_file}')
 
     # Create necessary directory structure.
     #
-    os.makedirs(avi_segment_scratch, 0o777, exist_ok=True)
-    os.makedirs(seg_output, 0o777, exist_ok=True)
+    logging.info('Setting up AVI directories.')
+    os.makedirs(avi_segment_scratch, permis, exist_ok=True)
+    os.makedirs(seg_output, permis, exist_ok=True)
     
     # Segmentation.
     #   Run the segmentation on the AVI file.
     #
-    seg_ff(avi, seg_output, SNR)
+    logging.info('Starting segmentation.')
+    seg_ff(avi, seg_output, SNR, segment_path)
           
     # Zip the segmentation output.
     # Create a new archive and use gzip to compress it.
     #
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': Start Tar')
-    tar_name = avi_segment_scratch + ".tar"
-    tar = "tar cf {tar} -C {dir_input} ." 
-    print ('\ttar.format: ', tar.format(tar=tar_name, dir_input=seg_output))
+    logging.info('Start tarring.')
+    tar_name = avi_segment_scratch + ".tar.gz"
+    tar = "tar czf {tar} -C {dir_input} ." 
+    logging.debug(tar.format(tar=tar_name, dir_input=seg_output))
     os.system(tar.format(tar=tar_name, dir_input=seg_output))
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': End Tar')
+    logging.info('End tarring.')
 
     shutil.rmtree(seg_output)          # remove datecode_s/
     shutil.rmtree(avi_segment_scratch) # remove datecode/
-
-    if (lazy_transfer):
-        os.remove(avi) # remove avi from the segment scratch
-
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': End local_main')
+    logging.info('End local_main.')
 
 #------------------------------------------------------------------------------
 # Fix Avi Names
 #
-def FixAviNames (avis):
+def FixAviNames(avis):
   n = 0
   for Name in avis:
     FixedName = Name.replace(' ', '-')
     if not (FixedName == Name):
-      os.rename (Name, FixedName)
+      os.rename(Name, FixedName)
       avis[n] = FixedName
     n += 1
     
@@ -234,112 +158,66 @@ def FixAviNames (avis):
 # main
 #
 if __name__ == "__main__":
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': Start main')
 
     # create a parser for command line arguments
     parser = argparse.ArgumentParser(description="Segmentation tool for the plankton pipeline. Uses ffmpeg and seg_ff to segment a video into crops of plankton")
-    parser.add_argument("-S", "--signal_to_noise", type=int, default=16, help="This is the Signal to Noise Ratio, often refered to as SNR")
-    parser.add_argument("-c", "--chunksize", type=int, default=5, help="How many images tars are allocated to a process at a time, default is 6 for smaller ammounts of avis make this smaller")
-    parser.add_argument("-p", "--num_processes", type=int, default=16, help="Number of processes you want to be running, this is how many files will be untared then found the measured concurrently. Adjust this based on the workload of the machine and the number of cores.")
+    parser.add_argument("-c", "--config", required = True, help = "Configuration ini file.")
     
-    parser.add_argument("-l", "--lazy_transfer_dir", default=None, help="If option entered files will be transfer from this directory to scratch space as they are needed")
-
-    # Add these if doing lazy transfer on remote host
-    parser.add_argument("-r", "--remote_login", default=None, help="This is the complete user@host for the file transfering host for the remote infastructure")
-    parser.add_argument("-P", "--port", type=int, default=22, help="The port for the transfer host")
-    parser.add_argument("-s", "--scratch_base", type=directory, required=True, help="This is the path for your fast ssd device storage.")
-
     # read in the arguments
     args = parser.parse_args()
-    scratch_base = os.path.abspath(args.scratch_base)
-    SNR = args.signal_to_noise
-    num_processes = args.num_processes
-    chunksize = args.chunksize
 
-    lazy_transfer_dir = args.lazy_transfer_dir
-    remote_login = args.remote_login
-    port = args.port
-
-    # Argparse Checks that were not able to be implemented in existing argparse functionality
-    remote_data = False
-    lazy_transfer = False
-    if lazy_transfer_dir != None:
-        lazy_transfer_dir = os.path.abspath(lazy_transfer_dir)
-        print("Video files for segmentation will be transfered when needed instead of all at once")
-        print("If this is a remote directory add the remote_login and port so that it is able to be transfered")
-        
-        lazy_transfer = True
-
-        if remote_login != None: 
-            # NOTE: ssh key to the remote host will need to be setup
-            cmd='ssh -p {remote_port} {remote_login} "[ -w {directory} ] ; exit"'
-            res = subprocess.call(cmd.format(remote_login=remote_login, remote_port=port, directory=lazy_transfer_dir), shell=True)
-            if (res != 0):
-                print("Error: Make sure this directory is valid on the remote host and that ssh key is setup from this machine to the host that files are being transfered from")
-                raise argparse.ArgumentTypeError("lazy_transfer_dir or remote host not valid")
-
-            remote_data = True
-
-        else:
-            if (not os.path.isdir(lazy_transfer_dir)):
-                raise argparse.ArgumentTypeError("lazy_transfer_dir not valid on local machine")
-
-    # intro print outs
-    print("Segmentation on:", scratch_base)
-    print("Number of processes:", num_processes)
-    print("Machine scratch", scratch_base)
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    
+    logging.config.fileConfig(config['logging']['config'], defaults={'date':datetime.datetime.now(),'path':config['general']['working_dir'],'name':'segmentation'})
+    logger = logging.getLogger('sLogger')
+    
+    ## Read in config options:
+    permis = int(config['general']['dir_permissions'])
+    SNR = int(config['segmentation']['signal_to_noise'])
+    num_processes = int(config['segmentation']['segment_processes'])
+    chunksize = int(config['segmentation']['chunksize'])
+    scratch_base = os.path.abspath(config['general']['working_dir'])
+    segment_path = config['segmentation']['segment']
+    
+    logger.debug(f"Segmentation on: {scratch_base}")
+    logger.debug(f"Number of processes: {num_processes}")
+    logger.debug(f"Machine scratch: {scratch_base}")
 
     # setup the output segmentation and measurements dir
     raw_scratch = scratch_base + "/raw"
-    measure_scratch = scratch_base + "/measurements"
-    short_segment_scratch = scratch_base + "/s"
     segment_scratch = scratch_base + "/segmentation"
-    os.makedirs(raw_scratch, 0o777,exist_ok=True)
-    os.makedirs(measure_scratch, 0o777,exist_ok=True)
-    os.makedirs(short_segment_scratch, 0o777,exist_ok=True)
-
+    short_segment_scratch = segment_scratch # TBK: make so the work is done in the final directories since we don't need to move them around.
+    
+    
+    logger.info("Setting up directories.")
+    os.makedirs(raw_scratch, permis, exist_ok=True)
+    os.makedirs(short_segment_scratch, permis, exist_ok=True)
+    
+    logger.info("Starting AVI loop.")
+    
     # Get the list of avi files if transfering lazily.
-    #
     avis = []
-    if lazy_transfer: 
-        if remote_data: # need to create avi array from list of avis in txt file
-            drive_file = os.path.join(scratch_base, "avi_list.txt")
-            get_file_list = "ssh -p {port} {remote_login} 'ls {raw_dir}/*.avi' > {drive_file}"
-            remote_raw = lazy_transfer_dir + "/raw"
-            os.system(get_file_list.format(port=port, remote_login=remote_login, raw_dir=remote_raw, drive_file=drive_file))
-            with open(drive_file, 'r') as d:
-                avis = [f[:-2] for f in d]
-            os.remove(drive_file)
-        else:
-            raw_dir = lazy_transfer_dir + "/raw"
-            avis = [os.path.join(raw_dir, avi) for avi in os.listdir(raw_dir) if avi.endswith(".avi")]
-    else:
-        avis = [os.path.join(raw_scratch, avi) for avi in os.listdir(raw_scratch) if avi.endswith(".avi")]
-        avis = FixAviNames (avis)
+    logging.info("Looking at avi filepaths without lazy_transfer")
+    avis = [os.path.join(raw_scratch, avi) for avi in os.listdir(raw_scratch) if avi.endswith(".avi")]
+    avis = FixAviNames(avis)
         
-        if (len(avis) == 0):
-            print("Error: no avi files found in machine_scratch/raw make sure avi files are there")
-            exit()
+    logging.debug("Found {length} AVI files.".format(length = len(avis)))
+    if (len(avis) == 0):
+        logging.error("No avi files found in machine_scratch/raw make sure avi files are there")
+        exit()
 
     # Parallel portion of the code.
-    #
+    logging.info("Starting multithreaded segmentation call.")
     p = Pool(num_processes)
 
-    # most efficient multiprocessing method, order doesn't matter so why not
-    # larger chunksize is supposed to be more efficient
-    #
-    # local_main
-    #
-    p.imap_unordered(local_main, avis, chunksize=chunksize)
+    # TBK:
+    for _ in tqdm.tqdm(p.imap_unordered(local_main, avis, chunksize=chunksize), total=len(avis)):
+        pass
 
     p.close()
     p.join() # blocks so that we can wait for the processes to finish
 
-    # move directory back to standard naming convention
-    try:
-        os.rename(short_segment_scratch, segment_scratch)
-    except: 
-        os.system("mv {src}/* {dst}; rm -rf {src}".format(src=short_segment_scratch, dst=segment_scratch))
-    print (datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), ': End main')
-    print("Finished Segmenting")
+    logger.debug("Finished segmentation.")
+    print("Finished Segmenting.")
     

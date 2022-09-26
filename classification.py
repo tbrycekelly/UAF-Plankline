de6c7b077987337d2139aaa0490409a68e41da8a
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2022-09-26 08:39:47 -0800

    Working

--- classification.py
+++ classification.py
@@ -18,25 +18,25 @@
 #
 #  This program is distributed WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# 
-#  This program is distributed under the 
+#
+#  This program is distributed under the
 #  GNU GPL v 2.0 or later license.
 #
-#  Any User wishing to make commercial use of the Software must contact the authors 
+#  Any User wishing to make commercial use of the Software must contact the authors
 #  or Oregon State University directly to arrange an appropriate license.
-#  Commercial use includes (1) use of the software for commercial purposes, including 
-#  integrating or incorporating all or part of the source code into a product 
-#  for sale or license by, or on behalf of, User to third parties, or (2) distribution 
-#  of the binary or source code to third parties for use with a commercial 
+#  Commercial use includes (1) use of the software for commercial purposes, including
+#  integrating or incorporating all or part of the source code into a product
+#  for sale or license by, or on behalf of, User to third parties, or (2) distribution
+#  of the binary or source code to third parties for use with a commercial
 #  product sold or licensed by, or on behalf of, User.
-# 
+#
 #  CITE AS:
-# 
+#
 #  Schmid MS, Daprano D, Jacobson KM, Sullivan CM, Briseño-Avena C, Luo JY, Cowen RK. 2021.
-#  A Convolutional Neural Network based high-throughput image 
+#  A Convolutional Neural Network based high-throughput image
 #  classification pipeline - code and documentation to process
-#  plankton underwater imagery using local HPC infrastructure 
-#  and NSF’s XSEDE. [Software]. Zenodo. 
+#  plankton underwater imagery using local HPC infrastructure
+#  and NSF’s XSEDE. [Software]. Zenodo.
 #  http://dx.doi.org/10.5281/zenodo.4641158
 #
 #########################################################
@@ -53,7 +53,7 @@ import argparse
 import glob
 import logging # TBK: logging module
 import configparser # TBK: To read config file
-import logging.config # TBK 
+import logging.config # TBK
 import tqdm # TBK
 import subprocess
 import datetime
@@ -66,13 +66,13 @@ from multiprocessing import Pool, Queue
 #
 def directory(arg):
     if os.path.isdir(arg):
-        if arg.endswith("/"): 
+        if arg.endswith("/"):
             arg = arg[:-1]
         return arg
     else:
         raise argparse.ArgumentTypeError("Not a valid directory path")
-        
-        
+
+
 #----------------------------------------------------------------------------------------
 # classify
 #
@@ -85,33 +85,36 @@ def classify(tar_file):
     image_dir = tar_file.replace(".tar.gz", "") # remove extension
     tar_identifier = os.path.basename(image_dir)
     os.makedirs(image_dir, permis, exist_ok = True)
-    
+    log_file = "{classification_dir}/{tar_identifier}-{date}.log".format(classification_dir = classification_dir, tar_identifier = tar_identifier, date = date)
+
     gpu_id = queue.get()
 
     logger.info("Starting on GPU {gpu_id}".format(gpu_id=gpu_id))
     logger.info(f'image_dir: {image_dir}')
     logger.info(f'tar_identifier: {tar_identifier}')
-    
+
     # Untar files
-    untar_cmd = 'tar --no-overwrite-dir -xzf {tar_file} -C {image_dir} --strip-components=4 --wildcards "*.png"' # TBK change strip-components to what you need.
+    untar_cmd = 'tar -xzf {tar_file} -C {image_dir} --strip-components=4 --wildcards "*.png"  >> {log_file} 2>&1' # TBK change strip-components to what you need.
     logger.debug('Untarring files: ' + untar_cmd.format(tar_file = tar_file, image_dir = image_dir))
     os.system(untar_cmd.format(tar_file = tar_file, image_dir = image_dir))
 
     # Perform classification.
-    log_file = "{classification_dir}/{tar_identifier}-{date}.log".format(classification_dir = classification_dir, tar_identifier = tar_identifier, date = date)
-    scnn_cmd  = "cd {scnn_dir}; nohup ./isiis_scnn -start {epoch} -stop {epoch} -unl {image_dir} -cD {gpu_target} > {log_file} 2>&1"
-    
+    scnn_cmd  = "cd {scnn_dir}; nohup ./isiis_scnn -start {epoch} -stop {epoch} -unl {image_dir} -cD {gpu_target} >> {log_file} 2>&1"
     logger.debug('Running SCNN: ' + scnn_cmd.format(scnn_dir = scnn_directory, epoch = epoch, image_dir = image_dir, gpu_target = gpu_id, log_file = log_file))
     logger.info('Start SCNN.')
-    
+
     os.system(scnn_cmd.format(scnn_dir = scnn_directory, epoch = epoch, image_dir = image_dir, gpu_target = gpu_id, log_file = log_file))
     logger.info('End SCNN.')
 
     # Move the csv file resulting from classification.
-    csv_path = glob.glob("{scnn_dir}/Data/plankton/*{tar_identifier}*".format(scnn_dir=scnn_directory, tar_identifier=tar_identifier))[0]
-    csv_file = "{classification_dir}/{tar_identifier}-{date}.csv".format(classification_dir=classification_dir, tar_identifier=tar_identifier, date=date)
-    shutil.move(csv_path, csv_file)
-
+    csv_path = glob.glob("{scnn_dir}/Data/plankton/*{tar_identifier}*".format(scnn_dir=scnn_directory, tar_identifier=tar_identifier))
+    if len(csv_path) > 0:
+        csv_path = csv_path[0]
+        csv_file = "{classification_dir}/{tar_identifier}-{date}.csv".format(classification_dir=classification_dir, tar_identifier=tar_identifier, date=date)
+        shutil.move(csv_path, csv_file)
+    else:
+        logger.error(f'No classification file found for {tar_identifier}')
+        
     # Clean directories to make space.
     shutil.rmtree(image_dir)
 
@@ -126,17 +129,17 @@ if __name__ == "__main__":
     # create a parser for command line arguments
     parser = argparse.ArgumentParser(description="Classification tool for managing the isiis_scnn processes")
     parser.add_argument("-c", "--config", required = True, help = "Configuration ini file.")
-    
+
 
     # read in the arguments
     args = parser.parse_args()
-    
+
     config = configparser.ConfigParser() # TBK
     config.read(args.config) # TBK
-    
+
     logging.config.fileConfig(config['logging']['config'], defaults={'date':datetime.datetime.now(),'path':config['general']['working_dir'],'name':'classification'}) # TBK
     logger = logging.getLogger('sLogger') # TBK
-    
+
     permis = int(config['general']['dir_permissions'])
     scnn_instances = int(config['classification']['scnn_instances'])
     scnn_directory = config['classification']['scnn_dir']
@@ -145,7 +148,7 @@ if __name__ == "__main__":
     scratch_base = os.path.abspath(config['general']['working_dir'])
 
     # segmentation_dir is where the input data is taken from
-    segmentation_dir = scratch_base + "/segmentation"  
+    segmentation_dir = scratch_base + "/segmentation"
     classification_dir = scratch_base + "/classification"
     os.makedirs(classification_dir, permis, exist_ok = True)
 
@@ -171,7 +174,7 @@ if __name__ == "__main__":
     logger.debug(f"SCNN Instances per GPU: {scnn_instances}")
     logger.debug(f"Total Processes: {num_processes}")
     logger.info(f"Identified {tar_length} tar.gz files")
-    
+
     # this is the parallel portion of the code
     p = Pool(num_processes)
 
@@ -185,11 +188,9 @@ if __name__ == "__main__":
     for _ in tqdm.tqdm(p.imap_unordered(classify, tars, chunksize=chunksize), total=len(tars)):
         pass
     #r = list(tqdm.tqdm(p.imap(classify, tars, chunksize = chunksize), total = len(tars)))
-    
+
     p.close()
     p.join() # blocks so that we can wait for the processes to finish
-    
+
     logger.debug("Finished classification.")
     print("Finished Classification.")
-    
-
--- segmentation.py
+++ segmentation.py
@@ -18,25 +18,25 @@
 #
 #  This program is distributed WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# 
-#  This program is distributed under the 
+#
+#  This program is distributed under the
 #  GNU GPL v 2.0 or later license.
 #
-#  Any User wishing to make commercial use of the Software must contact the authors 
+#  Any User wishing to make commercial use of the Software must contact the authors
 #  or Oregon State University directly to arrange an appropriate license.
-#  Commercial use includes (1) use of the software for commercial purposes, including 
-#  integrating or incorporating all or part of the source code into a product 
-#  for sale or license by, or on behalf of, User to third parties, or (2) distribution 
-#  of the binary or source code to third parties for use with a commercial 
+#  Commercial use includes (1) use of the software for commercial purposes, including
+#  integrating or incorporating all or part of the source code into a product
+#  for sale or license by, or on behalf of, User to third parties, or (2) distribution
+#  of the binary or source code to third parties for use with a commercial
 #  product sold or licensed by, or on behalf of, User.
-# 
+#
 #  CITE AS:
-# 
+#
 #  Schmid MS, Daprano D, Jacobson KM, Sullivan CM, Briseño-Avena C, Luo JY, Cowen RK. 2021.
-#  A Convolutional Neural Network based high-throughput image 
+#  A Convolutional Neural Network based high-throughput image
 #  classification pipeline - code and documentation to process
-#  plankton underwater imagery using local HPC infrastructure 
-#  and NSF’s XSEDE. [Software]. Zenodo. 
+#  plankton underwater imagery using local HPC infrastructure
+#  and NSF’s XSEDE. [Software]. Zenodo.
 #  http://dx.doi.org/10.5281/zenodo.4641158
 #
 #########################################################
@@ -56,7 +56,7 @@ import sys
 import shutil
 import argparse
 import logging # TBK: logging module
-import logging.config # TBK 
+import logging.config # TBK
 import configparser # TBK: To read config file
 import tqdm # TBK
 
@@ -66,20 +66,24 @@ import datetime
 
 def directory(arg):
     if os.path.isdir(arg):
-        if arg.endswith("/"): 
+        if arg.endswith("/"):
             arg = arg[:-1]
         return arg
     else:
         raise argparse.ArgumentTypeError("Not a valid directory path")
-        
+
 
 def seg_ff(avi, seg_output, SNR, segment_path):
     snr = str(SNR)
     epsilon = config['segmentation']['overlap']
+    delta = config['segmentation']['delta']
+    max_area = config['segmentation']['max_area']
+    min_area = config['segmentation']['min_area']
+
     segment_log = scratch_base + '/segmentation/segment_' + str(datetime.datetime.now()) + '.log'
     full_output = config['segmentation']['full_output']
-    
-    seg = f'nohup {segment_path} -i {avi} -n 1 -o {seg_output} -s {snr} -e {epsilon} {full_output} > "{segment_log}" 2>&1'
+
+    seg = f'nohup {segment_path} -i {avi} -n 1 -o {seg_output} -s {snr} -e {epsilon} -M {max_area} -m {min_area} -d {delta} {full_output} >> "{segment_log}" 2>&1'
     logger.info("Segmentation call: " + seg)
 
     os.system(seg)
@@ -94,7 +98,7 @@ def seg_ff(avi, seg_output, SNR, segment_path):
 #
 def local_main(avi):
     logging.info("Starting local_main.")
-    
+
     # setup all of the local paths for the avi
     avi_file = os.path.basename(avi) # get only the file part of the full path
     avi_date_code = os.path.splitext(avi_file)[0] # remove .avi
@@ -119,19 +123,19 @@ def local_main(avi):
     logging.info('Setting up AVI directories.')
     os.makedirs(avi_segment_scratch, permis, exist_ok=True)
     os.makedirs(seg_output, permis, exist_ok=True)
-    
+
     # Segmentation.
     #   Run the segmentation on the AVI file.
     #
     logging.info('Starting segmentation.')
     seg_ff(avi, seg_output, SNR, segment_path)
-          
+
     # Zip the segmentation output.
     # Create a new archive and use gzip to compress it.
     #
     logging.info('Start tarring.')
     tar_name = avi_segment_scratch + ".tar.gz"
-    tar = "tar czf {tar} -C {dir_input} ." 
+    tar = "tar czf {tar} -C {dir_input} ."
     logging.debug(tar.format(tar=tar_name, dir_input=seg_output))
     os.system(tar.format(tar=tar_name, dir_input=seg_output))
     logging.info('End tarring.')
@@ -151,7 +155,7 @@ def FixAviNames(avis):
       os.rename(Name, FixedName)
       avis[n] = FixedName
     n += 1
-    
+
   return avis
 
 #------------------------------------------------------------------------------
@@ -162,16 +166,16 @@ if __name__ == "__main__":
     # create a parser for command line arguments
     parser = argparse.ArgumentParser(description="Segmentation tool for the plankton pipeline. Uses ffmpeg and seg_ff to segment a video into crops of plankton")
     parser.add_argument("-c", "--config", required = True, help = "Configuration ini file.")
-    
+
     # read in the arguments
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
     config.read(args.config)
-    
+
     logging.config.fileConfig(config['logging']['config'], defaults={'date':datetime.datetime.now(),'path':config['general']['working_dir'],'name':'segmentation'})
     logger = logging.getLogger('sLogger')
-    
+
     ## Read in config options:
     permis = int(config['general']['dir_permissions'])
     SNR = int(config['segmentation']['signal_to_noise'])
@@ -179,7 +183,7 @@ if __name__ == "__main__":
     chunksize = int(config['segmentation']['chunksize'])
     scratch_base = os.path.abspath(config['general']['working_dir'])
     segment_path = config['segmentation']['segment']
-    
+
     logger.debug(f"Segmentation on: {scratch_base}")
     logger.debug(f"Number of processes: {num_processes}")
     logger.debug(f"Machine scratch: {scratch_base}")
@@ -188,20 +192,20 @@ if __name__ == "__main__":
     raw_scratch = scratch_base + "/raw"
     segment_scratch = scratch_base + "/segmentation"
     short_segment_scratch = segment_scratch # TBK: make so the work is done in the final directories since we don't need to move them around.
-    
-    
+
+
     logger.info("Setting up directories.")
     os.makedirs(raw_scratch, permis, exist_ok=True)
     os.makedirs(short_segment_scratch, permis, exist_ok=True)
-    
+
     logger.info("Starting AVI loop.")
-    
+
     # Get the list of avi files if transfering lazily.
     avis = []
     logging.info("Looking at avi filepaths without lazy_transfer")
     avis = [os.path.join(raw_scratch, avi) for avi in os.listdir(raw_scratch) if avi.endswith(".avi")]
     avis = FixAviNames(avis)
-        
+
     logging.debug("Found {length} AVI files.".format(length = len(avis)))
     if (len(avis) == 0):
         logging.error("No avi files found in machine_scratch/raw make sure avi files are there")
@@ -220,4 +224,3 @@ if __name__ == "__main__":
 
     logger.debug("Finished segmentation.")
     print("Finished Segmenting.")
-    

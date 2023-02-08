b9f2c7842bfee2644a91eec3b43195dc6a9e1824
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2023-02-08 13:27:13 -0900

    Improve cleanup and config inis

--- cleanup.py
+++ cleanup.py
@@ -47,24 +47,35 @@ if __name__ == "__main__":
         if os.path.isdir(working_dir + '/R/'):
             shutil.rmtree(working_dir + '/R/', ignore_errors = True)
     else:
+        def get_size(start_path = '.'):
+            total_size = 0
+            for dirpath, dirnames, filenames in os.walk(start_path):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    # skip if it is symbolic link
+                    if not os.path.islink(fp):
+                        total_size += os.path.getsize(fp)
+
+            return total_size
+
         print("No -f flag provided. Rerun with -f flag if you want to delete the following directories:")
         if os.path.isdir(fast_scratch + '/segment/'):
-            print(f"Directory: {fast_scratch + '/segment/':30} containing {(float(shutil.disk_usage(fast_scratch + '/segment/')[2])/10**9):.1f} GB")
+            print(f"Directory: {fast_scratch + '/segment/':30} containing {(get_size(fast_scratch + '/segment/')/10**9):.1f} GB")
         else:
             print(f"Directory: {fast_scratch + '/segment/':30} not found.")
         
         if os.path.isdir(working_dir + '/segmentation/'):
-            print(f"Directory: {working_dir + '/segmentation/':30} containing {(float(shutil.disk_usage(working_dir + '/segmentation/')[2])/10**9):.1f} GB")
+            print(f"Directory: {working_dir + '/segmentation/':30} containing {(get_size(working_dir + '/segmentation/')/10**9):.1f} GB")
         else:
             print(f"Directory: {working_dir + '/segmentation/':30} not found.")
         
         if os.path.isdir(working_dir + '/classification/'):
-            print(f"Directory: {working_dir + '/classification/':30} containing {(float(shutil.disk_usage(working_dir + '/classification/')[2])/10**9):.1f} GB")
+            print(f"Directory: {working_dir + '/classification/':30} containing {(get_size(working_dir + '/classification/')/10**9):.1f} GB")
         else:
             print(f"Directory: {working_dir + '/classification/':30} not found.")
         
         if os.path.isdir(working_dir + '/R/'):
-            print(f"Directory: {working_dir + '/R/':30} containing {(float(shutil.disk_usage(working_dir + '/R/')[2])/10**9):.1f} GB")
+            print(f"Directory: {working_dir + '/R/':30} containing {(get_size(working_dir + '/R/')/10**9):.1f} GB")
         else:
             print(f"Directory: {working_dir + '/R/':30} not found.")
         

e52c119f282e38d51299a6d892163e18d1da92ae
tbrycekelly <tbrycekelly@gmail.com>
2023-08-22 10:33:30 -0800

    update cleanup

--- cleanup.py
+++ cleanup.py
@@ -10,22 +10,14 @@ if __name__ == "__main__":
 
     # create a parser for command line arguments
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-c", "--config", required = True, help = "Configuration ini file.")
+    parser.add_argument("-d", "--directory", required = True, help = "Input directory containing ./raw/")
     parser.add_argument("-f", action = 'store_true')
 
     # read in the arguments
     args = parser.parse_args()
 
-    if os.path.isfile(args.config) == False:
-        print(f"No config file found called {args.config}. Aborting!")
-        exit()
-
-    config = configparser.ConfigParser()
-    config.read(args.config)
-
     ## Read in config options:
-    working_dir = config['general']['working_dir'] # TBK: Working directory
-    fast_scratch = config['segmentation']['fast_scratch'] # TBK: Fastest IO option for temporary files.
+    working_dir = os.path.abspath(args.directory)
 
     if args.f is True:
         if os.path.isdir(fast_scratch + '/segment/'):
@@ -52,10 +44,6 @@ if __name__ == "__main__":
             return total_size
 
         print("No -f flag provided. Rerun with -f flag if you want to delete the following directories:")
-        if os.path.isdir(fast_scratch + '/segment/'):
-            print(f"Directory: {fast_scratch + '/segment/':30} containing {(get_size(fast_scratch + '/segment/')/10**9):.1f} GB")
-        else:
-            print(f"Directory: {fast_scratch + '/segment/':30} not found.")
         
         if os.path.isdir(working_dir + '/segmentation/'):
             print(f"Directory: {working_dir + '/segmentation/':30} containing {(get_size(working_dir + '/segmentation/')/10**9):.1f} GB")

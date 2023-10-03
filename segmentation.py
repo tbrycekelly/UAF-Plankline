cc7bce0c95123d6ccf759445ed3448953d4340ee
tbrycekelly <tbrycekelly@gmail.com>
2023-10-03 12:43:11 -0800

    fix dir

--- segmentation.py
+++ segmentation.py
@@ -166,10 +166,10 @@ if __name__ == "__main__":
     ## Determine directories
     raw_dir = os.path.abspath(args.directory) # /media/plankline/Data/raw/Camera1/Transect1
     working_dir = raw_dir.replace("raw", "analysis") # /media/plankline/Data/analysis/Camera1/Transect1
-    working_dir = raw_dir.replace("Camera0/", "Camera0/segmentation/") # /media/plankline/Data/analysis/Camera1/Transect1
-    working_dir = raw_dir.replace("Camera1/", "Camera1/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
-    working_dir = raw_dir.replace("Camera2/", "Camera2/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
-    working_dir = raw_dir.replace("Camera3/", "Camera3/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("Camera0/", "Camera0/segmentation/") # /media/plankline/Data/analysis/Camera1/Transect1
+    working_dir = working_dir.replace("Camera1/", "Camera1/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("Camera2/", "Camera2/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("Camera3/", "Camera3/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
     
     segment_dir = working_dir + f" ({basename})" # /media/plankline/Data/analysis/segmentation/Camera1/segmentation/Transect1 (reg)
     fast_scratch = fast_scratch + "/segment-" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -195,7 +195,8 @@ if __name__ == "__main__":
     # Print config options to screen (TBK)
     print(f"Configureation file: {args.config}")
     print(f"Segmentation basename: {basename}")
-    print(f"Segmentation on: {segment_dir}")
+    print(f"Segmentation from: {raw_dir}")
+    print(f"Segmentation to: {segment_dir}")
     print(f"Scratch to: {fast_scratch}")
     print(f"Number of processes: {num_processes}")
     print(f"SNR: {SNR}")

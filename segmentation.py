6ef9866db0d2fe6a774acf53cb874c3ac9ba3538
tbrycekelly <tbrycekelly@gmail.com>
2023-10-03 12:50:48 -0800

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -166,10 +166,10 @@ if __name__ == "__main__":
     ## Determine directories
     raw_dir = os.path.abspath(args.directory) # /media/plankline/Data/raw/Camera1/Transect1
     working_dir = raw_dir.replace("raw", "analysis") # /media/plankline/Data/analysis/Camera1/Transect1
-    working_dir = working_dir.replace("Camera0/", "Camera0/segmentation/") # /media/plankline/Data/analysis/Camera1/Transect1
-    working_dir = working_dir.replace("Camera1/", "Camera1/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
-    working_dir = working_dir.replace("Camera2/", "Camera2/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
-    working_dir = working_dir.replace("Camera3/", "Camera3/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("camera0/", "camera0/segmentation/") # /media/plankline/Data/analysis/Camera1/Transect1
+    working_dir = working_dir.replace("camera1/", "camera1/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("camera2/", "camera2/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
+    working_dir = working_dir.replace("camera3/", "camera3/segmentation/") # /media/plankline/Data/analysis/Camera1/segmentation/Transect1
     
     segment_dir = working_dir + f"({basename})" # /media/plankline/Data/analysis/segmentation/Camera1/segmentation/Transect1(reg)
     fast_scratch = fast_scratch + "/segment-" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")

7b441eaebb6ea9e9e8715fbb55c475479464a999
tbrycekelly <tbrycekelly@gmail.com>
2023-10-05 11:59:56 -0800

    only untar crop png

--- classification.py
+++ classification.py
@@ -73,10 +73,10 @@ def classify(tar_file):
     
     # Untar files
     if config['general']['compress_output'] == 'True':
-        untar_cmd = f'tar -xzf "{tar_file}" -C "{tmp_dir}" --strip-components=4 --wildcards "*.png"  >> "{log_file}" 2>&1' # TBK change strip-components to what you need.
+        untar_cmd = f'tar -xzf "{tar_file}" -C "{tmp_dir}" --strip-components=4 --wildcards "*crop*.png"  >> "{log_file}" 2>&1' # TBK change strip-components to what you need.
         logger.debug('Untarring+unzipping files: ' + untar_cmd)
     else:
-        untar_cmd = f'tar -xf "{tar_file}" -C "{tmp_dir}" --strip-components=4 --wildcards "*.png"  >> "{log_file}" 2>&1' # TBK change strip-components to what you need.
+        untar_cmd = f'tar -xf "{tar_file}" -C "{tmp_dir}" --strip-components=4 --wildcards "*crop*.png"  >> "{log_file}" 2>&1' # TBK change strip-components to what you need.
         logger.debug('Untarring files: ' + untar_cmd)
     
     timer_untar = time()
